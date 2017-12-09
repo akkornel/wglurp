cc2f7b81c6e33d60480986fefaae1870b5519c1d
A. Karl Kornel <akkornel@stanford.edu>
2017-12-09 15:57:45 -0800

    Quiet things down, and add logging
    Quiet down alot of the update, install, and build stuff, so stderr
    becomes easier to see.
    
    Also, add status messages, to help figure out where we are.  That
    includes sending messages to Slack!

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,7 +35,11 @@ python3.6-dev # For building pyldap and systemd-python
 # Make sure we won't be prompted for stuff on install.
 export DEBIAN_FRONTEND=noninteractive
 
+# Get the Slack URL, for notifications
+export SLACK_URL=$(curl -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/attributes/SLACK_URL)
+
 # Set up 3rd-party repositories
+echo 'Adding apt repos'
 add-apt-repository -y ppa:jonathonf/python-3.6
 cat - > /etc/apt/sources.list.d/pgdg.list <<EOF
 deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main
@@ -43,65 +47,80 @@ EOF
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 
 # Update package lists, bring current packages up-to-date, and install stuff.
-apt-get update
-apt-get -y dist-upgrade
-apt-get -y install ${PERMANENT_PACKAGES[@]} ${TEMPORARY_PACKAGES[@]}
+curl -X POST --data-urlencode 'payload={"text": "Package Install..."}' ${SLACK_URL}
+echo 'Updating package lists'
+apt-get -q -q update
+echo 'Upgrading existing packages'
+apt-get -q -q -y dist-upgrade
+echo 'Installing packages'
+apt-get -q -q -y install ${PERMANENT_PACKAGES[@]} ${TEMPORARY_PACKAGES[@]}
 
 # Make a space for Git clones and other sources
 mkdir /root/git /root/src
 
 # Get setuptools, and build for Python 3.6
+curl -X POST --data-urlencode 'payload={"text": "SW install 1..."}' ${SLACK_URL}
+echo 'Installing Python 3.6 setuptools'
 git clone https://github.com/pypa/setuptools.git /root/git/setuptools
 cd /root/git/setuptools
 git checkout -q ${SETUPTOOLS_VERSION}
 python3.6 ./bootstrap.py
-python3.6 ./setup.py build
-python3.6 ./setup.py install
+python3.6 ./setup.py -q build
+python3.6 ./setup.py -q install
 
 # Get pip, and build for Python 3.6
+echo 'Installing Python 3.6 pip'
 git clone https://github.com/pypa/pip.git /root/git/pip
 cd /root/git/pip
 git checkout -q ${PIP_VERSION}
-python3.6 ./setup.py build
-python3.6 ./setup.py install
+python3.6 ./setup.py -q build
+python3.6 ./setup.py -q install
 
 # Install python-distutils-extra, needed by python-apt.
 cd /root/src
-apt-get source --download-only python-distutils-extra
+echo 'Installing python-distutils-extra from apt source'
+apt-get -q -q source --download-only python-distutils-extra
 tar -xzf python-distutils-extra*.tar.gz
 cd python-distutils-extra-*
-python3.6 ./setup.py build
-python3.6 ./setup.py install
+python3.6 ./setup.py -q build
+python3.6 ./setup.py -q install
 
 # Install python-apt
 cd /root/src
-apt-get source --download-only python-apt
+echo 'Installing python-apt from apt source'
+apt-get -q -q source --download-only python-apt
 tar -xJf python-apt*.tar.xz
 cd python-apt-*
-python3.6 ./setup.py build
-python3.6 ./setup.py install
+python3.6 ./setup.py -q build
+python3.6 ./setup.py -q install
 
 # Bring in Python stuff, using pip's hash checking
+curl -X POST --data-urlencode 'payload={"text": "SW install 2..."}' ${SLACK_URL}
+echo 'Installing Python packages from PyPi'
 pip3.6 install --require-hashes -r /root/bootstrap/requirements/ldap-client.txt
 
 # Get syncrepl_client, and build for Python 3.6
+curl -X POST --data-urlencode 'payload={"text": "SW install 3..."}' ${SLACK_URL}
+echo "Installing syncrepl_client from GitHub (version ${SYNCREPL_CLIENT_VERSION})"
 git clone git://github.com/akkornel/syncrepl.git /root/git/syncrepl
 cd /root/git/syncrepl
 git checkout -q ${SYNCREPL_CLIENT_VERSION}
-python3.6 ./setup.py build
-python3.6 ./setup.py install
+python3.6 ./setup.py -q build
+python3.6 ./setup.py -q install
 
 # Get wglurp, and build for Python 3.6
+echo "Installing WGLURP from GitHub (version ${WGLURP_VERSION})"
 git clone git://github.com/akkornel/wglurp.git /root/wglurp
 cd /root/wglurp
 git checkout -q ${WGLURP_VERSION}
-python3.6 ./setup.py build
-python3.6 ./setup.py install
-
+python3.6 ./setup.py -q build
+python3.6 ./setup.py -q install
 
 # Environment Setup
 
 # Set up a run directory
+curl -X POST --data-urlencode 'payload={"text": "Service install..."}' ${SLACK_URL}
+echo 'Setup wglurp-run-dir'
 cat - > /etc/systemd/system/wglurp-run-dir.service <<EOF
 [Unit]
 Description=Create the /run/wglurp directory tree
@@ -115,6 +134,7 @@ ExecStopPost=/bin/rm -rf /run/wglurp
 EOF
 
 # Set up a service that outputs environment variablaes
+echo 'Setup wglurp-env'
 cat - > /usr/sbin/wglurp-env.sh <<'EOF'
 #!/bin/bash
 
@@ -146,6 +166,7 @@ EOF
 # Cloud SQL Setup
 
 # Define a target for Cloud SQL
+echo 'Setup wglurp-sql'
 cat - > /etc/systemd/system/wglurp-sql.target <<EOF
 [Unit]
 Description=Represents WGLURP ready to make SQL connections.
@@ -156,6 +177,7 @@ WantedBy=multi-user.target
 EOF
 
 # Install and set up the Cloud SQL Proxy service
+echo 'Setup cloud_sql_proxy and cloud-sql-proxy'
 wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 \
      -O /usr/sbin/cloud_sql_proxy
 chmod a+x /usr/sbin/cloud_sql_proxy
@@ -182,6 +204,7 @@ EOF
 # Set up fstab entries for our data mount, and the key mount
 # NOTE: Only the key mount can be mounted automatically;
 # the data mount has to be decrypted first.
+echo 'Prep data mount'
 mkdir /mnt/data
 mkdir /mnt/data-key
 cat - >> /etc/fstab <<EOF
@@ -195,6 +218,7 @@ rmdir /usr/etc/wglurp.d
 ln -s /mnt/data/wglurp.conf /usr/etc/wglurp.conf
 ln -s /mnt/data/wglurp.conf.d /usr/etc/wglurp.d
 
+echo 'Setup wglurp-data-unlock and wglurp-data-mount'
 cat - > /etc/systemd/system/wglurp-data-unlock.service <<'EOF'
 [Unit]
 Description=Unlock the encrypted data partition
@@ -231,6 +255,7 @@ EOF
 
 # Keytab setup
 
+echo 'Setup wglurp-keytab'
 cat - > /etc/systemd/system/wglurp-ldap-keytab.service <<EOF
 [Unit]
 Description=Maintain a Krb5 credentials cache for LDAP
@@ -247,22 +272,28 @@ EOF
 # Clean Up and Reboot!
 
 # Trigger systemd reload, and enable appropriate services
+curl -X POST --data-urlencode 'payload={"text": "Notify and cleanup..."}' ${SLACK_URL}
+echo 'Notify Systemd and enable services'
 systemctl daemon-reload
 systemctl enable wglurp-sql.target
 
-
 # Clean up temporary packages
-apt-get -y purge ${TEMPORARY_PACKAGES[@]}
-apt-get -y autoremove
-apt-get -y clean
+echo 'Remove named temporary packages'
+apt-get -q -q -y purge ${TEMPORARY_PACKAGES[@]}
+echo 'Remove other temporary packages'
+apt-get -q -q -y autoremove
 
 # Clean up Git repos
+echo 'Clean up downloads'
 cd /root
 rm -rf /root/git
 rm -rf /root/src
+apt-get -q -q -y clean
 
 # If the NO_REBOOT metadata variable is defined, then just exit.
 # Otherwise, reboot so patches can take effect, and to start services!
+echo 'Complete!'
+curl -X POST --data-urlencode 'payload={"text": "Bootstrap complete!"}' ${SLACK_URL}
 if [ ${WGLURP_METADATA_NO_REBOOT:-z} = 'z' ]; then
     exit 0
 else
