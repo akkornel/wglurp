e07e060a8a4c064922e0efcb9023de3a5240168d
A. Karl Kornel <akkornel@stanford.edu>
2017-12-09 17:02:24 -0800

    Add empty __init__ files, for setuptools

---
+++ stanford_wglurp/db/__init__.py
@@ -0,0 +1,14 @@
+#!python
+# -*- coding: utf-8 -*-
+# vim: sw=4 ts=4 et
+
+# stanford-wglurp db base object.
+#
+# Copyright © 2017 The Board of Trustees of the Leland Stanford Jr. University.
+#
+# LICENSE
+
+# This file only exists so that setuptools will recognize it as a module to
+# be installed.
+
+pass
